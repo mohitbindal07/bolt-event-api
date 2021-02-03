4f94cc1bcf91186d144ebc1b58ce413f573b0aab
mohit <mohit.bindal16@gmail.com>
2021-02-03 20:46:05 +0530

    auth issue

--- src/main/java/com/example/demo/SlackAppController.java
+++ src/main/java/com/example/demo/SlackAppController.java
@@ -5,7 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.servlet.SlackAppServlet;
 
-@WebServlet("/slack/events")
+//@WebServlet("/slack/events")
 public class SlackAppController extends SlackAppServlet {
     public SlackAppController(App app) {
         super(app);
